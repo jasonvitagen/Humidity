90068c166a9747db40b0146c584cabb49d0fc5a0
Jason Cheng <jasoncheng@live.com.my>
2014-11-24 23:29:59 +0800

    COMMIT 3

--- config/auth.js
+++ config/auth.js
@@ -3,19 +3,19 @@ module.exports = {
 	facebookAuth: {
 		clientID     : '360743747418295',
 		clientSecret : 'c2ad59bc947e8591989360de6fecc6f2',
-		callbackURL  : 'http://mobile360.net/auth/facebook/callback'
+		callbackURL  : 'http://128.199.221.190/auth/facebook/callback'
 	},
 	
 	googleAuth: {
 		clientID     : '636394376141-f689jkrrtq3f3ce9lspp5hokm69cl2sm.apps.googleusercontent.com',
 		clientSecret : 'vDWcB-RsiEc-ZY1znLTzjSBW',
-		callbackURL  : 'http://mobile360.net/auth/google/callback'
+		callbackURL  : 'http://128.199.221.190/auth/google/callback'
 	},
 
 	renrenAuth : {
 		clientID     : 'e834917ff1c9450486bc878dae9ded48',
 		clientSecret : '0940202e5a16472b840911026b937f30',
-		callbackURL  : 'http://mobile360.net/auth/renren/callback'
+		callbackURL  : 'http://128.199.221.190/auth/renren/callback'
 	}
 
 };
